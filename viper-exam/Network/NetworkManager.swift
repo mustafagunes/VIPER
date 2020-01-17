839265bad3cc7fc07152fb76e80da5ab9f63e5b1
Mustafa GUNES <gunes149@gmail.com>
2020-01-17 12:57:45 +0300

    added network manager

---
+++ viper-exam/Network/NetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.swift
+//  viper-exam
+//
+//  Created by Mustafa GUNES on 17.01.2020.
+//  Copyright © 2020 Mustafa GUNES. All rights reserved.
+//
+
+import Foundation
+
+final class NetworkManager {
+    // MARK: - Definitions
+}
